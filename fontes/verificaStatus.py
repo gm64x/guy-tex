5035241b15cf78b6038307df46285ff2257b926b
Gabriel Miguel <67567474+gm64x@users.noreply.github.com>
2026-08-10 01:29:41 -0300

    fix: corrigir sintaxe de verificaStatus

--- fontes/verificaStatus.py
+++ fontes/verificaStatus.py
@@ -1,8 +1,6 @@
-void VerificaStatus(int x) {
-    if (x >= 10) {          // Nó de decisão (Predicado)
-        print("Aprovado");  // Caminho A
-    } else {
-        print("Reprovado"); // Caminho B
-    }
-    print("Fim");           // Nó de junção/fim
-}
+def VerificaStatus(x):
+    if x >= 10:              # Nó de decisão (Predicado)
+        print("Aprovado")    # Caminho A
+    else:
+        print("Reprovado")   # Caminho B
+    print("Fim")             # Nó de junção/fim
